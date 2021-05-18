1aa88cb8db1cc5ca66f5f79bc96643bc69db3b6c
doyoque <abiyoga1335@gmail.com>
2021-05-18 13:09:30 +0700

    test changes

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello world!, I am learning DevOps with AWS'));
+app.get('/', (req, res) => res.send('Hello world!, I am learning DevOps with AWS. Currently in steps 5'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
