a05e4fe0783d388e1b120fcfb2448b6eae9884ce
doyoque <abiyoga1335@gmail.com>
2021-05-18 13:33:54 +0700

    test comment

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
+// more comment sir
 app.get('/', (req, res) => res.send('Hello world!, I am learning DevOps with AWS. Currently in steps 5'));
 
 app.listen(port);
